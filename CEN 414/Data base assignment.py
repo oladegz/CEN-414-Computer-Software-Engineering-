07d8ca18285951d1e9364b246a029af7a31eee25
oladegz <148584970+oladegz@users.noreply.github.com>
2024-01-11 13:45:13 +0100

    Update Data base assignment.py

--- CEN 414/Data base assignment.py
+++ CEN 414/Data base assignment.py
@@ -1,34 +1,39 @@
 import cx_Oracle
-conStr='system/carefree@localhost:1521/xepdb1'
-#create a connection object
-conn=cx_Oracle.connect(conStr)
-#get a cursor object from the connection
+
+conStr = 'system/carefree@localhost:1521/xepdb1'
+
+# Create a connection object
+conn = cx_Oracle.connect(conStr)
+
+# Get a cursor object from the connection
 cur = conn.cursor()
 
-#insert table rows
-sqlTxt='insert into "CEN434".STUDENT_INFO (MATNO, NAME, PROGRAMME, ROOMNO, AGE)  values (:1, :2, :3, :4, :5)'
-cur.execute(sqlTxt,('20CF940202','Xavier Stalling','ENGLISH', 'B210', '16'))
+# Insert table rows
+sqlTxt = 'insert into "CEN434".STUDENT_INFO (MATNO, NAME, PROGRAMME, ROOMNO, AGE) values (:1, :2, :3, :4, :5)'
+cur.execute(sqlTxt, ('20CF940202', 'Xavier Stalling', 'ENGLISH', 'B210', '16'))
 
-#insert multiple rows
-dataTuples=[('20CF940302','Idowu Eniola','ENGLISH', 'C210', '17'),('20CK940302','Towoju Joy','ICE', 'F408', '18'),('20BC940302','Odetola Faith','ENGLISH', 'B110', '21')]
-sqlTxt='insert into "CEN434".STUDENT_INFO (MATNO, NAME, PROGRAMME, ROOMNO, AGE)  values (:1, :2, :3, :4, :5)'
+# Insert multiple rows
+dataTuples = [
+    ('20CF940302', 'Idowu Eniola', 'ENGLISH', 'C210', '17'),
+    ('20CK940302', 'Towoju Joy', 'ICE', 'F408', '18'),
+    ('20BC940302', 'Odetola Faith', 'ENGLISH', 'B110', '21')
+]
 cur.executemany(sqlTxt, dataTuples)
 
-#update table rows
-sqlTxt='update "CEN434".STUDENT_INFO set NAME=:1 where NAME=:2'
-cur.execute(sqlTxt,("Oluwafemi Tireni","AJIBOLA STANLEY"))
-#cur.execute executes the statement 
-#name1 replaces name2 in the table
+# Update table rows
+sqlTxt = 'update "CEN434".STUDENT_INFO set NAME=:1 where NAME=:2'
+cur.execute(sqlTxt, ("Oluwafemi Tireni", "AJIBOLA STANLEY"))
 
-#delete a record
-#def delete_student(MATNO, ROOMNO):
-#sqlTxt='delete from "CEN434".STUDENT_INFO where MATNO=:1'
-#cur.execute(sqlTxt,("20CF940302"))
-
+# Delete a record
+# Uncomment this section if you want to use it and replace the placeholder with the correct value
+# sqlTxt = 'delete from "CEN434".STUDENT_INFO where MATNO=:1'
+# cur.execute(sqlTxt, ('20CF940302',))
 
 conn.commit()
-# call the Connection.commit() method to apply the changes to the database. If you forget to call the Connection.commit() method, you will see that the change will not take effect.
+# Call the Connection.commit() method to apply the changes to the database.
+
 cur.close()
-#close the cursor object to avoid memory leaks
+# Close the cursor object to avoid memory leaks
+
 conn.close()
-#close the connection  object also
+# Close the connection object as well
